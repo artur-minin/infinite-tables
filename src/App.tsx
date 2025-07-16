f60a08cc77ac6412cd1afc5b195db40965d28e42
Artur Minin <artur.minindev@gmail.com>
2025-07-16 21:46:47 +0600

    feat: add drag and drop functionality to tables

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { useMemo } from 'react'
+import { useEffect, useMemo, useRef, useState } from 'react'
 
 import { createColumnHelper } from '@tanstack/react-table'
+import { SlotItemMapArray, Swapy, utils, createSwapy } from 'swapy'
 
 import Table, { Person } from '@/components/Table'
 
@@ -25,6 +26,30 @@ const defaultData: Person[] = [
   }
 ]
 
+type Table = {
+  id: number
+  name: string
+}
+
+const tables: Table[] = [
+  {
+    id: 1,
+    name: 'Table 1'
+  },
+  {
+    id: 2,
+    name: 'Table 2'
+  },
+  {
+    id: 3,
+    name: 'Table 3'
+  },
+  {
+    id: 4,
+    name: 'Table 4'
+  }
+]
+
 function App() {
   const columnHelper = createColumnHelper<Person>()
 
@@ -46,11 +71,64 @@ function App() {
     [columnHelper]
   )
 
+  const [items, setItems] = useState<Table[]>(tables)
+  const [slotItemMap, setSlotItemMap] = useState<SlotItemMapArray>(
+    utils.initSlotItemMap(items, 'name')
+  )
+  const slottedItems = useMemo(
+    () => utils.toSlottedItems(items, 'name', slotItemMap),
+    [items, slotItemMap]
+  )
+  const swapyRef = useRef<Swapy | null>(null)
+
+  const containerRef = useRef<HTMLDivElement>(null)
+
+  useEffect(
+    () =>
+      utils.dynamicSwapy(
+        swapyRef.current,
+        items,
+        'name',
+        slotItemMap,
+        setSlotItemMap
+      ),
+    [items]
+  )
+
+  useEffect(() => {
+    swapyRef.current = createSwapy(containerRef.current!, {
+      manualSwap: true,
+      animation: 'dynamic',
+      autoScrollOnDrag: true
+    })
+
+    swapyRef.current.onSwap((event) => {
+      setSlotItemMap(event.newSlotItemMap.asArray)
+    })
+
+    return () => {
+      swapyRef.current?.destroy()
+    }
+  }, [])
+
   return (
-    <div className="flex flex-wrap gap-[var(--gap-between-tables)] p-4">
-      {[1, 2, 3, 4].map((i) => (
-        <div key={`column-${i}`} className="min-w-[var(--table-width)] flex-1">
-          <Table columns={columns} initialData={defaultData} />
+    <div
+      ref={containerRef}
+      className="flex flex-wrap gap-[var(--gap-between-tables)] p-4"
+    >
+      {slottedItems.map(({ slotId, itemId, item }) => (
+        <div
+          key={slotId}
+          data-swapy-slot={slotId}
+          className="min-w-[var(--table-width)] flex-1 cursor-grab [&[data-swapy-highlighted]]:bg-green-200"
+        >
+          <div
+            key={itemId}
+            data-swapy-item={itemId}
+            className="[&[data-swapy-dragging]]:cursor-grabbing"
+          >
+            <Table columns={columns} initialData={defaultData} />
+          </div>
         </div>
       ))}
     </div>
